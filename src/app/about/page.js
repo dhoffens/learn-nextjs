18c83b864cfc4f865a0285f42e2e9d0400829dff
Daniel Hoffens <dhoffens@gmail.com>
2024-07-16 13:01:58 -0400

    routing setup

---
+++ src/app/about/page.js
@@ -0,0 +1,13 @@
+import React from "react";
+import './styles.css'
+
+const About = () => {
+    return (
+        <div className="container">
+            <h1>About Page</h1>
+            <p>This is the about page.</p>
+        </div>
+    );
+};
+
+export default About;
